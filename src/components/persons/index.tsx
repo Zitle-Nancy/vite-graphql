b4e15b6b5b6c6dbdcb6f9b23a7c4f5d7c82d9607
Zitle-Nancy <nancy.zitle@producepay.com>
2024-05-09 12:03:30 -0600

    [persons]: create the searchPerson function – render the person found with its data

--- src/components/persons/index.tsx
+++ src/components/persons/index.tsx
@@ -1,3 +1,6 @@
+import { gql, useLazyQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+
 interface AllPersonsProps {
   name: string;
 }
@@ -7,13 +10,72 @@ interface PersonsProps {
     allPersons: AllPersonsProps[];
   };
 }
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
+
 export const Persons = ({ data }: PersonsProps) => {
+  const [getPerson, result] = useLazyQuery(FIND_PERSON);
+  const [person, setPerson] = useState(null);
+
+  const searchPerson = (name: string) => {
+    getPerson({ variables: { nameToSearch: name } });
+  };
+
+  useEffect(() => {
+    if (result.data) {
+      setPerson(result.data.findPerson);
+    }
+  }, [result]);
+
+  if (person) {
+    const { name, phone, address } = person;
+    return (
+      <div>
+        <h2>{name}</h2>
+        <p>{phone}</p>
+        <p>
+          <strong>Ciudad: </strong>
+          {address?.city}{" "}
+        </p>
+        <p>
+          <strong>Dirección: </strong>
+          {address?.street}
+        </p>
+        <button onClick={() => setPerson(null)}>Close</button>
+      </div>
+    );
+  }
+
+  if (data === null) return null;
+
   return (
-    <ul>
-      {data &&
-        data.allPersons.map((person, index) => {
-          return <li key={index}>{person.name}</li>;
+    <>
+      <h1>Persons</h1>
+      <ul>
+        {data.allPersons.map((person, index) => {
+          return (
+            <li
+              style={{ margin: "10px 0" }}
+              key={index}
+              onClick={() => searchPerson(person.name)}
+            >
+              {person.name}
+            </li>
+          );
         })}
-    </ul>
+      </ul>
+    </>
   );
 };
