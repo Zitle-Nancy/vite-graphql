615a5040c053045b13d8212a1e0027d3cf23f95d
Zitle-Nancy <nancy.zitle@producepay.com>
2024-07-10 20:38:15 -0600

    [UpdatePhone]: create a useUpdatePhone - create UpdatePhoneForm

---
+++ src/hooks/useUpdatePhone.ts
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+
+import { UPDATE_PHONE } from "../graphql/mutations";
+import { ALL_PERSONS } from "../graphql/queries";
+
+export const useUpdatePhone = () => {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  // is an array because we say when want to call the useMutation()
+  const [updatePhone] = useMutation(UPDATE_PHONE, {
+    refetchQueries: [{ query: ALL_PERSONS }],
+    onError: (error) => {
+      setErrorMessage(error.graphQLErrors[0].message);
+      setTimeout(() => setErrorMessage(null), 5000);
+    },
+  });
+
+  return { updatePhone, errorMessage };
+};
